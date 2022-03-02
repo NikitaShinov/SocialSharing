dff0f52ecd28f2806dc920cc819ac46f5b34e221
Nikita Shinov <shinov_96@mail.ru>
2022-03-02 18:40:57 +0300

    Initial Commit

---
+++ SocialSharing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialSharing
+//
+//  Created by max on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
